ab05b058f1c19f4f8199c73056b14692430eb286
Ismailjoney <mohammadismailjoney@gmail.com>
2022-10-24 20:58:29 +0600

    server

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
+
+ 
 const categories = require(`./data/categories.json`)
+const news = require(`./data/news.json`)
 
 app.get(`/`, (req,res) => {
     res.send(`APi is running`);
@@ -15,4 +18,26 @@ app.get('/news-categories', (req,res) => {
     res.send(categories)
 })
 
-app.listen(port, () => console.log(`dragon news 5000`))
+app.get('/category/:id', (req,res) => {
+    id = req.params.id;
+    if(id === '08'){
+        res.send(news)
+    }else{
+        const categoriesNews = news.filter( n => n.category_id ===id);
+        res.send(categoriesNews)
+    }
+})
+
+app.get('/news', (req,res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req,res)=>{
+    const id = req.params.id;
+    const selectedNews = news.find( n => n._id === id);
+    res.send(selectedNews);
+  
+    
+})
+
+app.listen(port, () => console.log(`dragon news 5000`,port))
